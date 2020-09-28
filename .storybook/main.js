e2ccb21ca683f13ce8b4a3f7cae15b358896227e
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-09-27 21:05:38 -0300

    add support to storybook and preset-typescript config

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@stoybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
